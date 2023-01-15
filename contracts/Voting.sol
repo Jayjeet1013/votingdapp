ecf403f5c7502f4c1c084156866b8f37fa2209fc
Jayjeet1013 <jayjeet1013@gmail.com>
2023-01-15 21:48:14 +0530

    update smart  contract

---
+++ contracts/Voting.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+contract voting{
+      string  public question;
+   uint   sayyes=0;
+   uint   sayno=0;
+address owner;
+string loss;
+string winner;
+   constructor(){
+    owner = msg.sender;
+   }
+   modifier onlyowner(){
+      require(msg.sender == owner);
+      _;
+   }
+function askquesition( string memory _question) public  onlyowner returns(string memory) 
+{     
+     return question=_question;
+}
+function yes() public  {
+      sayyes++;
+}
+function no() public {
+    sayno++;
+}
+function result( ) public view  returns(string memory ){
+if(sayyes > sayno) {
+ return "win";
+}return "loss";
+}
+}
